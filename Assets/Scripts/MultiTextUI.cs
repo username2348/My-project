838da84dae3c5aa01852a0b3e6c610956e19df9d
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2024-04-01 18:26:42 +0300

    MultiText

---
+++ Assets/Scripts/MultiTextUI.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using YG;
+
+public class MultiText
+{
+    public string ruText;
+    public string enText;
+
+    public MultiText()
+    {
+        
+    }
+
+    public MultiText(string ruText, string enText)
+    {
+        this.ruText = ruText;
+        this.enText = enText;
+    }
+
+    public string GetText()
+    {
+        return MultiTextUI.lang == "ru" ? ruText : enText;
+    }
+}
+
+public class MultiTextUI : MonoBehaviour
+{
+    [SerializeField] private string ruText;
+    [SerializeField] private string enText;
+
+    private TMP_Text _text;
+
+    public static string lang;
+
+    private void Start()
+    {
+        _text = GetComponent<TMP_Text>();
+        if (_text != null)
+        {
+            _text.text = lang == "ru" ? ruText : enText;
+        }
+        else
+        {
+            var textMesh = GetComponent<TextMesh>();
+            if (textMesh != null)
+            {
+                textMesh.text = lang == "ru" ? ruText : enText;
+            }
+        }
+    }
+}
